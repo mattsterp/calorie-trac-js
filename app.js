0969acedd07e8e42db6dc46675c5a3773112c18e
Matthew Peters <mattsterp@gmail.com>
2018-12-06 13:24:30 -0500

    get and set data structure to UI.

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ const ItemCtrl = (function() {
     this.id = id;
     this.name = name;
     this.calories = calories;
+  };
 
-    // Data Structure / State
-    const data = {
-      items: [
-        { id: 0, name: "Steak Dinner", calories: 1200 },
-        { id: 1, name: "Cookie", calories: 400 },
-        { id: 2, name: "Eggs", calories: 300 }
-      ],
-      currentItem: null,
-      totalCalories: 0
-    };
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 1, name: "Cookie", calories: 400 },
+      { id: 2, name: "Eggs", calories: 300 }
+    ],
+    currentItem: null,
+    totalCalories: 0
   };
 
   // Public methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -30,8 +33,28 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: "#item-list"
+  };
+
   // Public methods
-  return {};
+  return {
+    populateItemList: function(items) {
+      let html = "";
+
+      items.forEach(function(item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+          <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+          </a>
+        </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
+  };
 })();
 
 // App Controller
@@ -39,7 +62,11 @@ const App = (function(ItemCtrl, UICtrl) {
   // Public methods
   return {
     init: function() {
-      console.log("Initializing App...");
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   };
 })(ItemCtrl, UICtrl);
